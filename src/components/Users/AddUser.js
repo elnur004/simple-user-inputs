f1c5e6b09240162cd9116aca53132ec7afedd801
elnur004 <lnur004@gmail.com>
2021-05-05 01:17:55 +0400

    update

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,10 @@
 const AddUser = (props) => {
   return (
     <form>
-      <label>Username</label>
-      <input type="text" htmlFor="username" id="username" />
-      <label>Age (Years)</label>
-      <input type="number" htmlFor="age" id="age" />
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" />
+      <label htmlFor="age">Age (Years)</label>
+      <input type="number" id="age" />
       <button type="submit">Add User</button>
     </form>
   );
