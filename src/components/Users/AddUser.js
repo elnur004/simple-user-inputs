ae193f1a25f13e6841562cabda3110213dfb4daa
elnur004 <lnur004@gmail.com>
2021-05-05 01:49:48 +0400

    handled user inputs as object on console

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+
 const AddUser = (props) => {
+  const [userNameInput, setUserNameInput] = useState('');
+  const [userAgeInput, setUserAgeInput] = useState(0);
+
+  const userNameHandler = (event) => {
+    setUserNameInput(event.target.value);
+  };
+
+  const userAgeHandler = (event) => {
+    setUserAgeInput(event.target.value);
+  };
+
+  const addUserNameHandler = (event) => {
+    event.preventDefault();
+
+    const userInput = {
+      userName: userNameInput,
+      userAge: +userAgeInput,
+    };
+    console.log(userInput);
+  };
+
   return (
-    <form>
+    <form onSubmit={addUserNameHandler}>
       <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
+      <input type="text" id="username" onInput={userNameHandler} />
       <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" />
+      <input type="number" id="age" onInput={userAgeHandler} />
       <button type="submit">Add User</button>
     </form>
   );
