a042ec84383c147edd8983b63108557adf2091e1
elnur004 <lnur004@gmail.com>
2022-12-20 01:21:28 +0400

    added styling

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,16 +1,17 @@
 import Card from './Card';
 import Button from './Button';
+import classes from './ErrorModal.modal.css';
 
 const ErrorModule = () => {
   return (
-    <Card>
-      <header>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
         <h2>Header</h2>
       </header>
-      <div>
+      <div className={classes.content}>
         <p>Error Message</p>
       </div>
-      <footer>
+      <footer className={classes.actions}>
         <Button>Okay</Button>
       </footer>
     </Card>
